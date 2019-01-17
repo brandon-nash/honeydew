169b657667101f8a56632e6d89c644e94fd40aa4
Brandon Nash <brandonnash1492@gmail.com>
2019-01-16 21:08:13 -0600

    Adding UI test for adding new Dew

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser } from 'protractor';
+import { browser, by } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -21,5 +21,17 @@ describe('workspace-project App', () => {
   it('should create Harvest Dews button', () => {
     page.navigateTo();
     expect(browser.isElementPresent(page.getHarvestDewsButton()));
-  })
+  });
+
+  it('should add new Dew when fields are populated', () => {
+    page.navigateTo();
+
+    page.getNewDewNameText().sendKeys('New Dew Test');
+    page.getNewDewDescriptionText().sendKeys('New Dew Description');
+    page.getAddNewDewButton().click();
+    
+    // This isn't totally valid if items had previously been added and are already 
+    // being loaded into memory. 
+    expect(page.getDewsList().count()).toBeGreaterThan(0);
+  });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -10,11 +10,23 @@ export class AppPage {
     return element(by.css('app-root h1')).getText();
   }
 
+  getNewDewNameText() {
+    return element(by.id('txtNewDewName'));
+  }
+
+  getNewDewDescriptionText() {
+    return element(by.id('txtNewDewDescription'));
+  }
+
   getAddNewDewButton() {
     return element(by.id('btnAddNewDew'));
   }
 
   getHarvestDewsButton() {
     return element(by.id('btnHarvestDews'));
+  }
+
+  getDewsList() {
+    return element.all(by.css('.dews li'));
   }
 }
